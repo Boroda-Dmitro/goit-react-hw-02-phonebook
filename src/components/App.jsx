2791da666a684dd499d95694077a0ea40ae74c3a
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-04-15 11:51:39 +0300

    prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,44 +15,49 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-
     filter: '',
   };
 
-  addContact = (newContact) => {
-    const { contacts} = this.state;
-    if (contacts.find(contact => 
-         contact.name.toLowerCase() === newContact.name.toLowerCase() || contact.number === newContact.number)
+  addContact = newContact => {
+    const { contacts } = this.state;
+    if (
+      contacts.find(
+        contact =>
+          contact.name.toLowerCase() === newContact.name.toLowerCase() ||
+          contact.number === newContact.number
+      )
     ) {
-      return toast.error(`${newContact.name} or ${newContact.number} is already in contacts`);
-      }
+      return toast.error(
+        `${newContact.name} or ${newContact.number} is already in contacts`
+      );
+    }
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
     }));
-  }
-
-
-  deleteContact = id => {
- this.setState(prevState => ({
-contacts: prevState.contacts.filter(contact => contact.id !== id)
- }))
   };
 
-  changeFilter = (e) => {this.setState({filter: e.target.value})}
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
 
   filteredContacts = () => {
-   const {filter, contacts } = this.state
+    const { filter, contacts } = this.state;
     return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  )};
-  
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
   render() {
     return (
       <div className={css.container}>
         <h1>Phonebook</h1>
-        <ContactForm
-          onSubmit={this.addContact}
-        />
+        <ContactForm onSubmit={this.addContact} />
 
         <h2>Contacts</h2>
         <Filter handleChange={this.changeFilter} value={this.state.filter} />
@@ -60,12 +65,8 @@ contacts: prevState.contacts.filter(contact => contact.id !== id)
           contacts={this.filteredContacts()}
           deleteContact={this.deleteContact}
         />
-        <ToastContainer 
-          position="top-center"
-          autoClose={2000} 
-          theme="dark"/>
+        <ToastContainer position="top-center" autoClose={2000} theme="dark" />
       </div>
-
     );
   }
 }
